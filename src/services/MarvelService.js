f1278f8322d3cb9aee6172944021654a814db964
isterya <vash.vybor.1@gmail.com>
2024-06-24 15:59:41 +0200

    v1.1: added error handling and random character loading element

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -25,7 +25,9 @@ class MarvelService {
    _transformCharacter = (char) => {
       return {
          name: char.name,
-         description: char.description,
+         description: char.description
+            ? `${char.description.slice(0, 210)}...`
+            : 'Nothing is known about that character.',
          thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
          homepage: char.urls[0].url,
          wiki: char.urls[1].url,
