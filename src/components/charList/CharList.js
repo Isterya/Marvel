e7dcc53b5ca622de5333a013d854766a93669f26
isterya <vash.vybor.1@gmail.com>
2024-06-26 14:28:24 +0200

    v1.3.1: added strict typing as propTypes

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -99,5 +100,9 @@ class CharList extends Component {
       );
    }
 }
+
+CharList.propTypes = {
+   onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
