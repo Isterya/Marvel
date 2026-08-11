2db21bd7864cac0bb139df7d3a62c30ed8ec9f90
isterya <vash.vybor.1@gmail.com>
2026-08-11 14:20:03 +0200

    fix: change offset state

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -30,7 +30,7 @@ const setContent = (process, Component, newItemLoading) => {
 const CharList = (props) => {
    const [charList, setCharList] = useState([]);
    const [newItemLoading, setnewItemLoading] = useState(false);
-   const [offset, setOffset] = useState(210);
+   const [offset, setOffset] = useState(0);
    const [charEnded, setCharEnded] = useState(false);
 
    const { getAllCharacters, process, setProcess } = useMarvelService();
